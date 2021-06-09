e6452aa1b2ddac97d9c542567b4a85051deabfe6
ALANVILASBOAS <alan07vb@gmail.com>
2021-06-09 02:16:31 -0300

    Primeira versão do frondend

---
+++ src/app/dashboard/dashboard.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import Stock from '../shared/models/stock-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+
+  readonly baseUrl = 'https://bootcamp-dio-santander5.herokuapp.com';
+
+  constructor(private http: HttpClient) { }
+
+  async getStocks(): Promise<Stock[]> {
+    return this.http.get<Stock[]>(`${this.baseUrl}/stock`).toPromise();
+  }
+}
